0d4ce99ea945db845e678289082dfe0f1901bccb
Carlos Filipe Sousa Pinho <carlos.p37@gmail.com>
2019-01-12 21:44:08 +0000

    Changing the Model

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -8,6 +8,13 @@ namespace Vidly.Models
     public class Customer
     {
         public int Id { get; set; }
+
         public string Name { get; set; }
+
+        public bool IsSubscribedToNewsLetter { get; set; }
+
+        public MembershipType MembershipType { get; set; }
+
+        public byte MembershipTypId { get; set; }
     }
 }
