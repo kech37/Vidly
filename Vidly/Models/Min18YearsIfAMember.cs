334ba4e0d59eb4edecf5a6066e56169e02465647
Carlos Filipe Sousa Pinho <carlos.p37@gmail.com>
2019-01-13 11:38:39 +0000

    Custom validation

---
+++ Vidly/Models/Min18YearsIfAMember.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Min18YearsIfAMember : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var customer = (Customer) validationContext.ObjectInstance;
+            if (customer.MembershipTypeId == 0 || customer.MembershipTypeId == 1)
+            {
+                return ValidationResult.Success;
+            }
+
+            if (customer.Birthdate == null)
+            {
+                return new ValidationResult("Birhdate is required");
+            }
+
+            var age = DateTime.Today.Year - customer.Birthdate.Value.Year;
+            if (age >= 18)
+            {
+                return ValidationResult.Success;
+            }
+            else
+            {
+                return new ValidationResult("Customer should be at leart 18 years old to go on a membership.");
+            }
+        }
+    }
+}
