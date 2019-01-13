48770c622e6ae30c76d1f0f5b5f2956c3ed24f02
Carlos Filipe Sousa Pinho <carlos.p37@gmail.com>
2019-01-13 16:59:09 +0000

    Section 6 Exercises

--- Vidly/Dtos/MovieDto.cs
+++ Vidly/Dtos/MovieDto.cs
@@ -3,11 +3,10 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
-using Vidly.Models;
 
 namespace Vidly.Dtos
 {
-    public class CustomerDto
+    public class MovieDto
     {
         public int Id { get; set; }
 
@@ -15,11 +14,14 @@ namespace Vidly.Dtos
         [StringLength(255)]
         public string Name { get; set; }
 
-        public bool IsSubscribedToNewsletter { get; set; }
+        [Required]
+        public byte GenreId { get; set; }
 
-        public byte MembershipTypeId { get; set; }
+        public DateTime DateAdded { get; set; }
 
-        //[Min18YearsIfAMember]
-        public DateTime? Birthdate { get; set; }
+        public DateTime ReleaseDate { get; set; }
+
+        [Range(1, 20)]
+        public byte NumberInStock { get; set; }
     }
 }
