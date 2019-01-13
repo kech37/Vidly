9bb82df81565b0f31485e3cf91b7424abacc5db3
Carlos Filipe Sousa Pinho <carlos.p37@gmail.com>
2019-01-13 00:35:27 +0000

    form labels

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -20,6 +20,7 @@ namespace Vidly.Models
 
         public byte MembershipTypeId { get; set; }
 
+        [Display(Name = "Date of birth")]
         public DateTime? Birthdate { get; set; }
     }
 }
