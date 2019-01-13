c1aa80b388b55302253ef7e9975d2b513884da63
Carlos Filipe Sousa Pinho <carlos.p37@gmail.com>
2019-01-13 18:23:40 +0000

    Section 7 Exercises

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -24,11 +24,9 @@ namespace Vidly.Controllers
         }
 
         // GET: Movies
-        public ActionResult Index()
+        public ViewResult Index()
         {
-            var movies = _context.Movies.Include(m => m.Genre).ToList();
-
-            return View(movies);
+            return View();
         }
 
         public ActionResult Details(int id)
