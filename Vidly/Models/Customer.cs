1d34fa3ba72c55bc494c71c968b600e3d00ca72b
Carlos Filipe Sousa Pinho <carlos.p37@gmail.com>
2019-01-13 00:54:33 +0000

    Drop-down lists

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -18,6 +18,7 @@ namespace Vidly.Models
 
         public MembershipType MembershipType { get; set; }
 
+        [Display(Name="Membership Type")]
         public byte MembershipTypeId { get; set; }
 
         [Display(Name = "Date of birth")]
