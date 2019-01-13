b7f54cbc77a66c12634989237b429a30929b3a45
Carlos Filipe Sousa Pinho <carlos.p37@gmail.com>
2019-01-13 11:42:41 +0000

    Refactoring Magic Numbers

--- Vidly/Models/MembershipType.cs
+++ Vidly/Models/MembershipType.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,8 +8,12 @@ namespace Vidly.Models
 {
     public class MembershipType
     {
+        public static readonly byte Unknown = 0;
+        public static readonly byte PayAsYouGo = 1;
+
         public byte Id { get; set; }
 
+        [Required]
         public string Name { get; set; }
 
         public short SignUpFee { get; set; }
